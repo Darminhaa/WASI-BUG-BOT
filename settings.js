683d54c6efedcab8b92af0386f8d2398300865a3
Darminhaa <niyafattrrrpa@gmail.com>
2024-09-10 16:13:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,23 +3,23 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "94760552994"
+global.ownername = "🐛 MR KAVIZ "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
-global.location = "Pakistan, Lahore"
+global.location = "SRI LANKA, KAGALLE"
 
 global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
+global.ownername = ' MR Kaviz ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'WASI'
+global.author = 'DK'
 
 //console view/theme
 global.themeemoji = '🪀'
-global.wm = "WASI TECH"
+global.wm = "DK TECH"
 
 //theme link
 global.link = 'https://whatsapp.com'
